1fb7d23eb9b0c32992687b7826fbf170f28ea5ce
ViniciusCSantos <viniciuscsantosoficial@gmail.com>
2023-10-15 11:33:08 -0300

    Feat: adds the 2 horizontal cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import home1 from "@/assets/home-1.png";
+import styled from "styled-components";
+import Cards from "@/components/Cards";
+
+const Container = styled.div`
+  padding: 24px;
+  width: 100%;
+`
+
+const Intro = styled.div`
+  position: relative;
+  overflow: hidden;
+  margin-bottom: 32px;
+
+  > img {
+    width: 100%;
+    max-width: 668px;
+    border-radius: 20px;
+    user-select: none;
+    pointer-events: none;
+  }
+`
+
+const IntroTitle = styled.h2`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <Container>
+      <Intro>
+        <Image src={home1} alt="" width={668} height={180}/>
+
+        <IntroTitle>Find Your Community on Daccord</IntroTitle>
+      </Intro>
+
+      <Cards />
+    </Container>
   )
 }
---
+++ src/components/Cards.tsx
@@ -0,0 +1,150 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import styled from "styled-components"
+import FeaturedCard1 from "@/assets/featured-card-1.png"
+import FeaturedCard2 from "@/assets/featured-card-2.png"
+import MemberIcon from "@/assets/member.png"
+import MemberIconOnline from "@/assets/member-online.png"
+
+const CardContainer = styled.div`
+  
+`
+
+const CardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-bottom: 12px;
+
+  > a {
+    color: rgba(255, 255, 255, 0.55);
+    text-align: right;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+`
+
+const Title = styled.h3`
+  color: #FFF;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 28px;
+`
+
+const Card = styled.div`
+  position: relative;
+
+  img {
+    user-select: none;
+    pointer-events: none;
+  }
+`
+
+const CardTitle = styled.h4`
+  position: absolute;
+  left: 12px;
+  bottom: 74px;
+  font-size: 22px;
+  font-weight: 600;
+`
+const CardSubtitle = styled.p`
+  position: absolute;
+  left: 12px;
+  bottom: 34px;
+  color: rgba(255, 255, 255, 0.55);
+  font-family: inherit;
+  font-size: 13px;
+  font-weight: 400;
+`
+
+const CardInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  left: 12px;
+  right: 12px;
+  bottom: 12px;
+  color: rgba(255, 255, 255, 0.25);
+  font-family: inherit;
+  font-size: 12px;
+  font-weight: 400;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  }
+`
+
+const Cards = () => {
+  return (
+    <>
+      <CardContainer>
+        <CardHeader>
+          <Title>Featured Community</Title>
+
+          <Link href="/see-all">See All</Link>
+        </CardHeader>
+
+        <CardWrapper>
+          <Card>
+            <Image src={FeaturedCard1} alt="" width={320}/>
+
+            <CardTitle>Virtual Reality</CardTitle>
+            <CardSubtitle>A community for VR and novices alike, regular and friendly chat.</CardSubtitle>
+
+            <CardInfo>
+              <span>
+                <Image src={MemberIcon} alt="" width={14} height={14}/>
+
+                5,678 Online
+              </span>
+
+              <span>
+                <Image src={MemberIconOnline} alt="" width={14} height={14}/>
+
+                345,678 Members
+              </span>
+            </CardInfo>
+          </Card>
+
+          <Card>
+            <Image src={FeaturedCard2} alt="" width={320}/>
+
+            <CardTitle>Game Play</CardTitle>
+            <CardSubtitle>Always a new challenge. Great place to make new friends.</CardSubtitle>
+
+            <CardInfo>
+              <span>
+                <Image src={MemberIcon} alt="" width={14} height={14}/>
+
+                28,628 Online
+              </span>
+
+              <span>
+                <Image src={MemberIconOnline} alt="" width={14} height={14}/>
+
+                527,955 Members
+              </span>
+            </CardInfo>
+          </Card>
+        </CardWrapper>
+      </CardContainer>
+    </>
+  )
+}
+
+export default Cards
