5d5b9d9aab0becf24d528dd466e5d19a7f1a2936
ViniciusCSantos <viniciuscsantosoficial@gmail.com>
2024-11-06 11:09:36 -0300

    Feat: add new members cards
    Add members card to the right sidebar and enable border color customization in HexagonBorder component.

--- src/components/HexagonBorder.tsx
+++ src/components/HexagonBorder.tsx
@@ -1,7 +1,6 @@
-function HexagonBorder({src, width, height}: {src?: string, width: number, height: number}) {
-  const color1 = "#A73EE7";
-  const color2 = "#00EBFF";
-
+function HexagonBorder({src, width, height, color1, color2}: {src?: string, width: number, height: number, color1?: string, color2?: string}) {
+  if(!color1) color1 = "#A73EE7";
+  if(!color2) color2 = "#00EBFF";
 
   return(
     <div style={{ position: 'relative', width, height }} className="hexagon-border">
@@ -30,12 +29,12 @@ function HexagonBorder({src, width, height}: {src?: string, width: number, heigh
 
         <path
           d="M26.7128 6.8453L16.8564 1.1547C15.6188 0.440169 14.094 0.440169 12.8564 1.1547L3 6.8453C1.7624 7.55983 1 8.88034 1 10.3094V21.6906C1 23.1197 1.7624 24.4402 3 25.1547L12.8564 30.8453C14.094 31.5598 15.6188 31.5598 16.8564 30.8453L26.7128 25.1547C27.9504 24.4402 28.7128 23.1197 28.7128 21.6906V10.3094C28.7128 8.88034 27.9504 7.55983 26.7128 6.8453Z"
-          stroke="url(#paint0_linear)"
+          stroke={`url(#linear-paint-${color1}-${color2})`}
           strokeWidth={.5}
         />
         
         <defs>
-          <linearGradient id="paint0_linear" x1="0.856384" y1="9.5" x2="26.3564" y2="26" gradientUnits="userSpaceOnUse">
+          <linearGradient id={`linear-paint-${color1}-${color2}`} x1="0.856384" y1="9.5" x2="26.3564" y2="26" gradientUnits="userSpaceOnUse">
             <stop stopColor={color1} />
             <stop offset="1" stopColor={color2} />
           </linearGradient>
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -6,6 +6,7 @@ import MailIcon from "./icons/MailIcon";
 import BellIcon from "./icons/BellIcon";
 import ChatIcon from "./icons/ChatIcon";
 import SettingsIcon from "./icons/SettingsIcon";
+import HexagonBorder from "./HexagonBorder";
 
 const Aside = styled.aside`
   background: #2C2F48;
@@ -23,7 +24,54 @@ const Options = styled.div`
   }
 `
 
+const Title = styled.h3`
+  display: flex;
+  justify-content: space-between;
+  font-size: 15px;
+  margin: 28px 16px 16px;
+  color: rgba(255, 255, 255, 0.55);
+  font-weight: 400;
+
+  span {
+    color: rgba(255, 255, 255, 0.25);
+  }
+`
+
+const Cards = styled.ul`
+  display: grid;
+  gap: 8px;
+  padding: 0 16px;
+`
+
+const Card = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  background: #393D5D;
+  border-radius: 5px;
+`
+
+const CardInfo = styled.div`
+  margin-left: 12px;
+
+  h3 {
+    color: #fff;
+    font-size: 14px;
+    font-weight: 400;
+  }
+
+  p {
+    color: rgba(255, 255, 255, 0.25);
+    font-size: 13px;
+  }
+`
+
 const UserProfile = () => {
+  const userProfile1 = "https://media.istockphoto.com/id/1386479313/pt/foto/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=1024x1024&w=is&k=20&c=8ZanqZPUGnq35YEyoEN9706LLYZYGf3JVu7BnsJ28hc="
+  const userProfile2 = "https://media.istockphoto.com/id/1437816897/pt/foto/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring-or.jpg?s=2048x2048&w=is&k=20&c=mkmHzbsIlTMJCGDlBD4PAP5MmCaRD05baBA9ECQMGo4="
+  const userProfile3 = "https://media.istockphoto.com/id/1407759041/pt/foto/confident-happy-beautiful-hispanic-student-girl-indoor-head-shot-portrait.jpg?s=2048x2048&w=is&k=20&c=X22vaOhrm7VMb09TfjMpEwbZuDN7JL3cojVaJDjcU7w=";
+  const userProfile4 = "https://media.istockphoto.com/id/1325359218/pt/foto/portrait-of-a-young-african-man-at-studio-high-fashion-male-model-in-colorful-bright-neon.jpg?s=2048x2048&w=is&k=20&c=NBpZFJmQLN4a-unXU5H5iQniX7m8sRPmJHyUO7yfDhE=";
+
   return (
     <Aside>
       <Options>
@@ -34,6 +82,50 @@ const UserProfile = () => {
       </Options>
 
       <ProfileInfo />
+
+      <Title>
+        New Members
+
+        <span>See All</span>
+      </Title>
+
+      <Cards>
+        <Card>
+          <HexagonBorder src={userProfile1} width={44} height={44} color1="#FFFFFF" color2="#FFFFFF"/>
+
+          <CardInfo>
+            <h3>Anne Couture</h3>
+            <p>5 min ago</p>
+          </CardInfo>
+        </Card>
+
+        <Card>
+          <HexagonBorder src={userProfile2} width={44} height={44} color1="#FFFFFF" color2="#FFFFFF"/>
+
+          <CardInfo>
+            <h3>Miriam Soleil</h3>
+            <p>20 min ago</p>
+          </CardInfo>
+        </Card>
+
+        <Card>
+          <HexagonBorder src={userProfile3} width={44} height={44} color1="#FFFFFF" color2="#FFFFFF"/>
+
+          <CardInfo>
+            <h3>Marie Laval</h3>
+            <p>35 min ago</p>
+          </CardInfo>
+        </Card>
+
+        <Card>
+          <HexagonBorder src={userProfile4} width={44} height={44} color1="#FFFFFF" color2="#FFFFFF"/>
+
+          <CardInfo>
+            <h3>Mark  Morain</h3>
+            <p>40 min ago</p>
+          </CardInfo>
+        </Card>
+      </Cards>
     </Aside>
   )
 }
